cea7a840bf59faeb3119aa8152dafe834bd3cb63
satish <satishpawar.vision@gmail.com>
2021-12-02 10:42:21 +0530

    Add project files.

---
+++ ASP.NET MVC Application/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET_MVC_Application.Startup))]
+namespace ASP.NET_MVC_Application
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
